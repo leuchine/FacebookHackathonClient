c7f9c25a8b77e2ad1ae32314f33132681d97297e
Matouš Skála <skala.matous@gmail.com>
2016-09-03 15:35:14 +0800

    add map view

--- give2get/www/js/controllers.js
+++ give2get/www/js/controllers.js
@@ -1,6 +1,64 @@
 angular.module('starter.controllers', [])
 
-.controller('PowerUpCtrl', function($scope) {})
+.controller('PowerUpCtrl', function($scope) {
+
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  navigator.geolocation.getCurrentPosition(function (pos) {
+    var lat = pos.coords.latitude;
+    var lng = pos.coords.longitude;
+    console.log('position:', lat, lng);
+
+    var latLng = new google.maps.LatLng(lat, lng);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    google.maps.event.addListenerOnce($scope.map, 'idle', function() {
+      var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+      });
+
+      var infoWindow = new google.maps.InfoWindow({
+          content: "Here I am!"
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open($scope.map, marker);
+      });
+    });
+
+  }, function (err) {
+    console.log('err', err);
+  }, options);
+
+/*
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
+    console.log('position:' + position);
+
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+  */
+
+})
 
 .controller('HuntCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
